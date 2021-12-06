fe9dab785fd892d75b57c73f17e3f76361a49ad1
ilhamsyamsuddin <32873409+ilhamsyamsuddin@users.noreply.github.com>
2021-12-06 13:32:31 +0000

    perbaikan xor function tahap 1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<string.h>
 #include <unordered_map>
+#include<vector>
 #include <map>
 //#include "kubus.h"
 #include "utilities.h"
@@ -10,18 +11,13 @@ using namespace std;
 
 int main(){
   
-  string message = "0123456789ABBA9876543210";
-  string key = "7416D123471D5030AA649817EE091FE8";
-  string try_key = "52A0F468C58D08C90D9B8AFEF7B5308C7A2B4587D48B266AAA9EE6";
-  string pt = hex2bin(message);//96 bit plaintext
+  string message = "0D901B8FF4EE3A118FD61297E8BAE6DFF15F63C571D7EC7B3B861C";//54x4 = 216 bit
+  string try_key = "52A0F468C58D08C90D9B8AFEF7B5308C7A2B4587D48B266AAA9EE6";//54* 4 = 216 bit
   string bit_key = hex2bin("F");//128 bit key
   string face_key = hex2bin("A");
     string ans = "";
-  for(short i =0; i<4;i++){
-    ans+= "1";
-  }
-  string bit = xor_hex("A","F");
-  cout<<bit<<endl;
+  string bit = xor_hex("0D901B8FF4EE3A118FD61297E8BAE6DFF15F63C571D7EC7B3B861C","52A0F468C58D08C90D9B8AFEF7B5308C7A2B4587D48B266AAA9EE6");
+  cout<<bit<<"yes"<<endl;
   Cube box;
   box.fillCube(try_key);
   //box.printAllFaces();
