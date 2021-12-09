5308d517e20a9b4f84a35a75d6b8946c068bc7fc
ilhamsyamsuddin <32873409+ilhamsyamsuddin@users.noreply.github.com>
2021-12-09 23:18:45 +0000

    Menyelesaikan key Generation dan tahap pertama encryption dan decryption

--- main.cpp
+++ main.cpp
@@ -13,26 +13,51 @@ int main(){
   
   string message = "0D901B8FF4EE3A118FD61297E8BAE6DFF15F63C571D7EC7B3B861C";//54x4 = 216 bit
   string key = "52A0F468C58D08C90D9B8AFEF7B5308C7A2B4587D48B266AAA9EE6";//54* 4 = 216 bit
-  string ans;
-  ans = addRoundKey(message,key);
+  //Key Generation
+  vector<string>roundKeys;
+  roundKeys.push_back(key);
+  for(int a = 1;a<=10;a++){
+    Cube keyBox;
+    keyBox.fillCube(key);
+    keyBox.scramble(roundKeys[a-1]);
+    string roundKey = keyBox.getContent();
+    roundKey = subBytes(roundKey);
+    roundKeys.push_back(roundKey);
+  }
+
+  for(int a = 0;a<roundKeys.size();a++){
+    cout<<a<<": "<<roundKeys[a]<<endl;
+  }
+  string text;
+  text = addRoundKey(message,key);
   /*for(int a = 0;a<54;a++){
-    ans.push_back(xor_hex(message[a],key[a]));
+    text.push_back(xor_hex(message[a],key[a]));
   }*/
-  cout<<"XOR: "<<ans<<endl;
-  ans = subBytes(ans);
-  cout<<ans<<endl;
+  cout<<"XOR: "<<text<<endl;
+  text = subBytes(text);
+  cout<<"S-box: "<<text<<endl;
 
+  for(int a = 1;a<=10;a++){
+    Cube box;
+    box.fillCube(text);
+    box.scramble(roundKeys[a-1]);
+    text = box.getContent();
+  }
+  cout<<"scrambled: "<<text<<endl;
 
-  Cube box;
-  box.fillCube(ans);
-
-
-  string text = box.getContent();
-  cout<<text<<endl;
-  box.scramble(message);
+  for(int a = 10;a>=1;a--){
+    Cube box;
+    box.fillCube(text);
+    box.solve(roundKeys[a-1]);
+    text = box.getContent();
+  }
+  cout<<"solved: "<<text<<endl;
+  /*box.scramble(message);
+  box.rotateDown90();
   text = box.getContent();
-  cout<<text<<endl;
-  box.solve(message);
+  cout<<"scrambled: "<<text<<endl;
+  //box.solve(message);
+  box.rotateDown270();
   text = box.getContent();
-  cout<<text<<endl;
+  cout<<"Solved: "<<text<<endl;*/
 }
